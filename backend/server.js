c17dd7b5c9e6d0fc51ce013480a2b746be010ac9
Srijan Pratap Srivastava <85446215+SRIJANPRATAPSRIVASTAVA@users.noreply.github.com>
2024-08-13 00:41:43 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path")
 const bannerRoutes = require("./routes/banner.routes")
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -8,12 +9,21 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-// test
-app.use('/test', (req, res) => {
-    res.status(200).json({
-        success: true
+
+const __dirname1 = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "../frontend/dist")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
     })
-});
+} else {
+    app.use('/', (req, res) => {
+        res.status(200).json({
+            success: true
+        })
+    });
+}
 
 // endpoints
 app.use("/api", bannerRoutes)
